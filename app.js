89c9bda78c9c48f6b18d79381f39fb56c47ff653
Takafumi Nakahara <kodawaripoteto@gmail.com>
2017-11-06 22:39:41 +0900

    TouchDesignerとの連携部分(送信データのファイル書き出しとOSCによる完了通知)

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var socketio = require('socket.io');
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var client = new osc.Client('127.0.0.1', 3000);
+var rimraf = require('rimraf');
+var jsonfile = require('jsonfile');
 
+var cnt = 0;
+var td_osc = new osc.Client('127.0.0.1', 3000);
 var provider_ip = "192.168.3.93";
 var provider_osc = new osc.Client(provider_ip, 4000);
 
@@ -20,10 +23,14 @@ io.sockets.on('connection', function(socket) {
         console.log(data);
 				let sendData = JSON.stringify(data);
 
-      	client.send('/YabaCoin', sendData);
-
+        console.log("TRY SAVE TO DATABASE");
         sendData = string_to_utf8_bytes(sendData);
         provider_osc.send('/YabaCoin', sendData);
+        console.log("SAVED TO DATABASE");
+
+        console.log("TRY WRITE FILE");
+        textfileWrite(data);
+
     });
 });
 
@@ -50,9 +57,74 @@ function	string_to_utf8_bytes(text){
 }
 
 
-/*var client = new osc.Client('127.0.0.1', 3000);
+//tochDesignerに渡すデータを書き込む関数
+function textfileWrite(data) {
 
-setInterval(function(){
-  client.send('/recieve', "Hello world!");
-  console.log("messege send");
-}, 5000);*/
+  data = data.value;
+
+  var jsondata = {};
+
+  rimraf('./data/textsdata', function (err){
+
+    //データの削除に失敗したらエラーを出力し終了
+    if (err) {
+        console.error("ERROR at rmdir");
+        console.error(err);
+        process.exit(1);
+    }
+    console.error("Deleted datafolder");
+
+    //ファイルを作成
+    fs.mkdir('./data/textsdata', function (err) {
+      //ファイル作成に失敗したらエラーを出力し終了
+      if (err) {
+          console.error("ERROR at mkdir");
+          console.error(err);
+          process.exit(1);
+      }
+      console.error("Created datafolder");
+
+      //JSONデータに入るオブジェクトの中身を入れる
+      jsondata["text"] = {
+            "age" : data.age,
+            "gender" : data.gender,
+            "yaba_event" : data.YE,
+            "likedislike" : data.likedislike,
+            "joysad" : data.joysad,
+            "angerfear" : data.angerfear,
+            "scalar" : data.scalar
+      }
+
+      console.log(jsondata);
+
+      //ファイルの書き込み
+      writeFile("./data/textsdata/text.txt", data.YE);
+
+      jsonfile.writeFile("./data/data.json", jsondata, {
+          encoding: 'utf-8',
+          replacer: null,
+          spaces: null
+      }, function (err) {
+          console.log("Saved jsondata");
+          td_osc.send('/YabaCoin', "done");
+          console.log("ALL DONE");
+      });
+
+    });
+  }); //END of rimraf(function{});
+}
+
+
+
+//ファイルの書き込み関数
+function writeFile(path, data) {
+  fs.writeFile(path, data, function (err) {
+    if (err) {
+      throw err;
+    }
+    if(cnt == 0){
+      cnt ++;
+      console.log("writeFile Pass");
+    }
+  });
+}
