d242cadd4bb41d93ac22db4618b7fdde20f3fecc
tetsuya88 <yata.gara38@gmail.com>
2017-11-08 22:32:47 +0900

    styling is over

---
+++ htdocs/js/animation.js
@@ -0,0 +1,38 @@
+(function() {
+  var ripple, ripples, RippleEffect,loc, cover, coversize, style, x, y, i, num;
+
+  //クラス名rippleの要素を取得
+  ripples = document.querySelectorAll('.ripple');
+
+  //位置を取得
+  RippleEffect = function(e) {
+  ripple = this;//クリックされたボタンを取得
+ 	cover = document.createElement('span');//span作る
+ 	coversize = ripple.offsetWidth;//要素の幅を取得
+  loc = ripple.getBoundingClientRect();//絶対座標の取得
+  x = e.pageX - loc.left - window.pageXOffset - (coversize / 2);
+  y = e.pageY - loc.top - window.pageYOffset - (coversize / 2);
+  pos = 'top:' + y + 'px; left:' + x + 'px; height:' + coversize + 'px; width:' + coversize + 'px;';
+
+  //spanを追加
+  ripple.appendChild(cover);
+  cover.setAttribute('style', pos);
+  cover.setAttribute('class', 'rp-effect');//クラス名追加
+
+  //しばらくしたらspanを削除
+  setTimeout(function() {
+    var list = document.getElementsByClassName( "rp-effect" ) ;
+    for(var i =list.length-1;i>=0; i--){//末尾から順にすべて削除
+    	list[i].parentNode.removeChild(list[i]);
+  }}, 2000)};
+  for (i = 0, num = ripples.length; i < num; i++) {
+    ripple = ripples[i];
+    ripple.addEventListener('mousedown', RippleEffect);
+  }
+}());
+
+var topView = document.getElementsByClassName('top')[0];
+var startButton = document.getElementsByClassName('startButton')[0];
+startButton.addEventListener("click", function (e) {
+  topView.classList.add('closeTop');
+});
